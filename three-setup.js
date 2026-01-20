d8f06606e4e8e74f20144dfc8e22b52577ffbee0
Nayatrei <36463159+Nayatrei@users.noreply.github.com>
2026-01-20 14:43:25 -0500

    3d fix

---
+++ three-setup.js
@@ -0,0 +1,9 @@
+import * as THREE from './vendor/three/build/three.module.js';
+import { SVGLoader } from './vendor/three/examples/jsm/loaders/SVGLoader.js';
+import { OBJExporter } from './vendor/three/examples/jsm/exporters/OBJExporter.js';
+import * as BufferGeometryUtils from './vendor/three/examples/jsm/utils/BufferGeometryUtils.js';
+
+window.THREE = THREE;
+THREE.BufferGeometryUtils = BufferGeometryUtils;
+window.SVGLoader = SVGLoader;
+window.OBJExporter = OBJExporter;
